7a5c716396407558742da1b3c6716a52a2043646
Иван <34417022+ellementul@users.noreply.github.com>
2020-01-10 11:46:48 +0500

    Fixed main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 module.exports = {
 	CrHoarder: require("./Hoarder.js"),
 	CrCommunicator: require("./Commun.js"),
-	CrRouter: require("./Router.js")
+	CrRouter: require("./Router.js"),
+	CrMultiComm: require("./MultiCommun.js"),
 };
